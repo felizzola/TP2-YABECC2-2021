bd07ae9edebbdfd9ea9a92b812b214dff22607e7
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-10-05 04:02:36 -0300

    testing users en cliente

--- mongo/cliente.js
+++ mongo/cliente.js
@@ -1,6 +1,7 @@
 import { getInventors, getInventor, addInventor, updateInventor, deleteInventor } from "./inventors.js";
+import {getUserbyemail, getUsers, getUser, pushUser, updateUser, deleteUser} from "./users.js";
 
-try {
+//try {
     //console.log(await getInventors());
     //console.log(await getInventor('6153b0641a071df5278126ed'));
     // console.log(await addInventor({
@@ -14,8 +15,33 @@ try {
     //     last: "Gutierrez 2",
     //     year: 2000
     // }));
-    console.log(await deleteInventor('6155087614a2dfea1336f666'));
-} catch (error) {    
+//    console.log(await deleteInventor('6155087614a2dfea1336f666'));
+//} catch (error) {    
+//    console.log(error.message);
+//    console.log("error en el cliente");
+//}
+
+try{
+    //console.log(await getUsers());
+    //console.log(await getUser('615be75f1e86b12dc9dcfcb0'));
+    //console.log(await getUserbyemail('art_parkinson@gameofthron.es'));
+
+    //console.log(await pushUser({
+    //    name: "Lionel",
+    //    email: "lionel-email@gmail.com",
+    //    password: user.password
+    //}));
+
+    //console.log(await updateInventor({
+    //    _id: "615be75f1e86b12dc9dcfcb1",
+    //    name: "Carolina",
+    //    email: "Pecas",
+    //    password: 'how?'
+    //}));
+
+    console.log(await deleteUser('615be75f1e86b12dc9dcfcad'));
+} catch (error){
     console.log(error.message);
-    console.log("error en el cliente");
+    console.log("¡ERROR! Leé el mensaje error para solucionarlo.");
 }
+
